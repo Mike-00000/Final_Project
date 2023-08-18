681217e361f676d71573485f27e77769296d9462
Mike <mt.mail.mt@gmail.com>
2023-08-18 15:07:02 +0300

    Commiting changements about styling the title page

--- curse-of-the-twins/src/components/Home.js
+++ curse-of-the-twins/src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './home.css';
 import Introduction from './Introduction'; 
+import couverture from '../images/couverture.jpg';
+import arabesque from "../images/arabesque.png";
+
 
 
 const Home = () => {
@@ -75,54 +78,103 @@ const Home = () => {
   };  
   
 
-  return (
-    <div>
-      <h1>The Curse of the Twins</h1>
-      {isAuthenticated ? (
-        <>
-          {showIntroduction ? (
-            <Introduction />
-          ) : (
-            <>
-              <form onSubmit={handleLogin}>
-                <label>
-                  Username:
-                  <input type="text" value={loginUsername} onChange={(e) => setLoginUsername(e.target.value)} />
-                </label>
-                <label>
-                  Password:
-                  <input type="password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} />
-                </label>
-                <button type="submit">Login</button>
-              </form>
-              <form onSubmit={handleRegister}>
-                <label>
-                  Username:
-                  <input type="text" value={registerUsername} onChange={(e) => setRegisterUsername(e.target.value)} />
-                </label>
-                <label>
-                  Password:
-                  <input type="password" value={registerPassword} onChange={(e) => setRegisterPassword(e.target.value)} />
-                </label>
-                <label>
-                  Email:
-                  <input type="text" value={registerEmail} onChange={(e) => setRegisterEmail(e.target.value)} />
-                </label>
-                <button type="submit">Register</button>
-              </form>
-              <button onClick={handleNewGame}>New Game</button>
-              <button onClick={() => setNewGame(false)}>Continue</button>
-            </>
-          )}
-        </>
-      ) : (
-        <>
-          <button onClick={() => setNewGame(true)}>New Game</button>
-          <button onClick={() => setNewGame(false)}>Continue</button>
-        </>
-      )}
+//   return (
+//     <div>
+//       <h1>The Curse of the Twins</h1>
+//       {isAuthenticated ? (
+//         <>
+//           {showIntroduction ? (
+//             <Introduction />
+//           ) : (
+//             <>
+              // <form onSubmit={handleLogin}>
+              //   <label>
+              //     Username:
+              //     <input type="text" value={loginUsername} onChange={(e) => setLoginUsername(e.target.value)} />
+              //   </label>
+              //   <label>
+              //     Password:
+              //     <input type="password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} />
+              //   </label>
+              //   <button type="submit">Login</button>
+              // </form>
+//               <form onSubmit={handleRegister}>
+//                 <label>
+//                   Username:
+//                   <input type="text" value={registerUsername} onChange={(e) => setRegisterUsername(e.target.value)} />
+//                 </label>
+//                 <label>
+//                   Password:
+//                   <input type="password" value={registerPassword} onChange={(e) => setRegisterPassword(e.target.value)} />
+//                 </label>
+//                 <label>
+//                   Email:
+//                   <input type="text" value={registerEmail} onChange={(e) => setRegisterEmail(e.target.value)} />
+//                 </label>
+//                 <button type="submit">Register</button>
+//               </form>
+//               <button onClick={handleNewGame}>New Game</button>
+//               <button onClick={() => setNewGame(false)}>Continue</button>
+//             </>
+//           )}
+//         </>
+//       ) : (
+//         <>
+//           <button onClick={() => setNewGame(true)}>New Game</button>
+//           <button onClick={() => setNewGame(false)}>Continue</button>
+//         </>
+//       )}
+//     </div>
+//   );
+// }
+
+return (
+  <div className={isAuthenticated ? 'authenticated' : 'not-authenticated'}>
+    <div className="title-container">
+      <div className="arabesque" style={{ backgroundImage: `url(${arabesque})` }} />
+      <h1 className="title" >The Curse of Emma</h1>
+      <div className="arabesque mirror" style={{ backgroundImage: `url(${arabesque})` }} />
     </div>
-  );
+      
+      
+      {isAuthenticated ? (
+          <>
+              {showIntroduction ? (
+                  <Introduction />
+              ) : (
+                <>
+                  <div className="buttons-container">
+                      <div className="button-box">
+                          <button className='buttton' onClick={handleNewGame}>New Game</button>
+                      </div>
+                      <div className="button-box">
+                          <button className='buttton' onClick={() => setNewGame(false)}>Continue</button>
+                      </div>
+                  </div>
+
+                  <div className="image-container">
+                      <img src={couverture} alt="Original" />
+                      <img src={couverture} alt="Mirror" className="mirror-image" />
+                  </div>
+                </>
+              )}
+          </>
+      ) : (
+          <>
+              <div className="forms-container">
+                  <form onSubmit={handleLogin}>
+                      /* ... Votre code existant pour le formulaire de connexion ... */
+                  </form>
+                  <form onSubmit={handleRegister}>
+                      /* ... Votre code existant pour le formulaire d'inscription ... */
+                  </form>
+              </div>
+          </>
+      )}
+  </div>
+);
+
+
 }
 
 export default Home;
