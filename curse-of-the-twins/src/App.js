9dd5b863e1860ddde453a6ab786238dc236c8e1e
Mike <mt.mail.mt@gmail.com>
2023-09-27 17:28:16 +0300

    Commiting some tests about debuging

--- curse-of-the-twins/src/App.js
+++ curse-of-the-twins/src/App.js
@@ -61,3 +61,99 @@ const App = () => {
 };
 
 export default App;
+
+
+// ___________________________________________________________________________________
+
+
+
+// import React, { useState, useEffect, createContext } from 'react';
+// import Home from './components/Home';
+// import Introduction from './components/Introduction';
+// import PassageWindow from './components/PassageWindow';
+// import ChoiceMenu from './components/ChoiceMenu';
+
+// export const AppContext = createContext("");
+
+// const App = () => {
+//   const [currentComponent, setCurrentComponent] = useState('Home');
+//   const [passageIdfordb, setPassageIdfordb] = useState(0);
+//   const [passageId, setPassageId] = useState(0);
+//   const [passageId2, setPassageId2] = useState(0);
+//   const [introductionPassages, setIntroductionPassages] = useState([]);
+//   const [yourChoicesArray, setYourChoicesArray] = useState([]);
+
+//   useEffect(() => {
+//     const fetchIntroductionPassages = async () => {
+//       try {
+//         const response = await fetch(`http://localhost:3000/passages`);
+//         const data = await response.json();
+//         console.log("Données de passage récupérées avec succès :", data); // Ajoutez ce log pour vérifier les données récupérées
+//         if (Array.isArray(data)) {
+//           setIntroductionPassages(data);
+//         }
+//         console.log("testttttt2", setIntroductionPassages);
+//       } catch (error) {
+//         console.error('Error fetching introduction passages:', error);
+//       }
+//     };
+
+//     // const fetchChoices = async () => {
+//     //   try {
+//     //     const response = await fetch(`http://localhost:3000/choices`);
+//     //     const data = await response.json();
+//     //     console.log("Choices data:", data);
+
+//     //     if (Array.isArray(data)) {
+//     //       setYourChoicesArray(data);
+//     //     }
+//     //   } catch (error) {
+//     //     console.error('Error fetching choices:', error);
+//     //   }
+//     // };
+
+//     fetchIntroductionPassages();
+//     // fetchChoices();
+//   }, []);
+
+//   const handleNextComponent = () => {
+//         console.log("testttttttttttttttttttttt", currentComponent);
+//         if (currentComponent === 'Home') {
+//           setCurrentComponent('Introduction');
+//         } else if (currentComponent === 'Introduction') { 
+//           setCurrentComponent('PassageWindow');
+//         } else if (currentComponent === 'PassageWindow') {
+//           setCurrentComponent('ChoiceMenu');
+//         } else {
+    
+//         }
+//         setPassageId(0)
+//         setPassageIdfordb(passageIdfordb+1)
+//         setCurrentComponent('PassageWindow');
+//       };
+
+//       const handleOptionSelected = (selectedChoice) => {
+    
+//             setPassageIdfordb(selectedChoice.nextPassageId);
+//             console.log("New passageId:", selectedChoice.nextPassageId); // Ajoutez ce log pour vérifier la mise à jour du passageId
+//             setCurrentComponent('PassageWindow'); 
+//             console.log("Option sélectionnée :", selectedChoice.text);
+//           };
+
+//   return (
+//     <AppContext.Provider value={{ handleNextComponent, passageIdfordb, setPassageIdfordb, passageId, setPassageId, passageId2, setPassageId2 }}>
+//       <div className="App">
+//         {currentComponent === 'Home' && <Home onNext={handleNextComponent} introductionPassages={introductionPassages} key={1} />}
+//         {currentComponent === 'Introduction' && <Introduction handleNextComponent={handleNextComponent} introductionPassages={introductionPassages} key={2} />}
+//         {currentComponent === 'PassageWindow' && (
+//           <PassageWindow passageId2={passageId2} onNextPassage={handleNextComponent} introductionPassages={introductionPassages} key={3} />
+//         )}
+//         {currentComponent === 'ChoiceMenu' && (
+//           <ChoiceMenu choices={yourChoicesArray} onOptionSelected={handleOptionSelected} key={4} />
+//         )}
+//       </div>
+//     </AppContext.Provider>
+//   );
+// };
+
+// export default App;
--- curse-of-the-twins/src/components/Introduction.js
+++ curse-of-the-twins/src/components/Introduction.js
@@ -161,3 +161,215 @@ const Introduction = (props) => {
 };
 
 export default Introduction;
+
+// ____________________________________________________________________________________
+
+// import React, { useState, useEffect, useContext } from 'react';
+// import './introduction.css';
+// import illustration1 from '../images/parchment-intro.jpg';
+// import illustration2 from '../images/forest-cabin.jpg';
+// import illustration3 from '../images/loan_shark-and-father.jpg';
+// import illustration4 from '../images/bandits-attack-father.jpg';
+// import illustration5 from '../images/hidden-girl.jpg';
+// import illustration6 from '../images/girl-escape.jpg';
+// import { animateScroll as scroll } from 'react-scroll';
+// // import { handleNextComponent } from '../App';
+// import { AppContext } from '../App';
+
+// const backgroundIllustration = illustration1;
+
+// const illustrations = [
+//   illustration2,
+//   illustration3,
+//   illustration4,
+//   illustration5,
+//   illustration6,
+// ];
+
+// const charactersPerFrameArray = [50, 30, 20]; 
+
+// const Introduction = (props) => {
+//   console.log("Introduction props", props);
+//   const [displayText, setDisplayText] = useState('');
+//   const [currentIndex, setCurrentIndex] = useState(0);
+//   const [currentIllustration, setCurrentIllustration] = useState(illustrations[0]);
+//   const [introductionPassages, setIntroductionPassages] = useState([]);
+//   const [passageId, setPassageId] = useState(1);
+//   const [showNextButton, setShowNextButton] = useState(true);
+
+//   const [isMusicPlaying, setIsMusicPlaying] = useState(true);
+//   const { handleNextComponent, passageIdfordb } = useContext(AppContext);
+//   const formatTextWithLineBreaks = (text) => {
+//     const formattedText = text.replace(/<br>/g, '<br/>');
+//     return formattedText;
+//   };
+
+//   const textRef = React.useRef(null);
+
+//   useEffect(() => {
+//     const fetchIntroductionPassages = async () => {
+//       try {
+//         const response = await fetch(`http://localhost:3000/passages/${passageIdfordb}`);
+//         const data = await response.json();
+//         console.log("testttttt", data);
+
+//         if (Array.isArray(data)) {
+//           setIntroductionPassages(data);
+//         }
+//         console.log("testttttt2", setIntroductionPassages);
+
+//       } catch (error) {
+//         console.error('Error fetching introduction passages:', error);
+//       }
+//     };
+
+//     fetchIntroductionPassages();
+//   }, [passageId]);
+
+//   useEffect(() => {
+//     const passageText = introductionPassages?.find((passage) => passage.id === passageId)?.passage_text || '';
+//     console.log("testttttt3", passageText);
+
+//     const intervalId = setInterval(() => {
+//       const charactersPerFrame = charactersPerFrameArray.find((value, index) => index === passageId - 1) || 100;
+//       if (currentIndex < passageText.length) {
+//         setDisplayText((prevText) => prevText + passageText[currentIndex]);
+//         setCurrentIndex((prevIndex) => prevIndex + 1);
+//       } else {
+//         clearInterval(intervalId);
+//         if (currentIllustration !== illustrations[illustrations.length - 1]) {
+//           const DELAY_BEFORE_NEXT_PASSAGE = 6700;
+//           setTimeout(() => {
+//             const currentIllustrationIndex = illustrations.indexOf(currentIllustration);
+//             setCurrentIllustration(illustrations[currentIllustrationIndex + 1]);
+//             setCurrentIndex(0);
+//             setDisplayText('');
+//             setPassageId((prevPassageId) => prevPassageId + 1);
+//           }, DELAY_BEFORE_NEXT_PASSAGE);
+//         } else {
+//           setShowNextButton(true); 
+//         }
+//       }
+//     }, 75);
+
+//     return () => clearInterval(intervalId);
+//   }, [currentIndex, currentIllustration, introductionPassages, passageId]);
+
+//   useEffect(()=> {
+//     textRef.current.scrollTop = textRef.current.scrollHeight
+//   },[displayText])
+
+//   return (
+//     <div className="container">
+//       <div className="illustration-container" style={{ backgroundImage: `url(${currentIllustration})` }} />
+//       <div className="text-container">
+//         <div className="parchment aaa" id="parchment-id" ref={textRef}>
+//           <p className="text"  dangerouslySetInnerHTML={{ __html: formatTextWithLineBreaks(displayText)   }} />
+//         {showNextButton && <button onClick= {handleNextComponent} className="next-button">Next</button>}
+//         </div>
+//       </div>
+//       <audio src="/RPG intro 100 BPM2.wav" autoPlay={isMusicPlaying} />
+//     </div>
+//   );
+// };
+
+// export default Introduction;
+
+
+// ________________________________________________________________________________
+
+
+// import React, { useState, useEffect, useContext } from 'react';
+// import './introduction.css';
+// import illustration1 from '../images/parchment-intro.jpg';
+// import illustration2 from '../images/forest-cabin.jpg';
+// import illustration3 from '../images/loan_shark-and-father.jpg';
+// import illustration4 from '../images/bandits-attack-father.jpg';
+// import illustration5 from '../images/hidden-girl.jpg';
+// import illustration6 from '../images/girl-escape.jpg';
+// import { animateScroll as scroll } from 'react-scroll';
+// // import { handleNextComponent } from '../App';
+// import { AppContext } from '../App';
+
+// const backgroundIllustration = illustration1;
+
+// const illustrations = [
+//   illustration2,
+//   illustration3,
+//   illustration4,
+//   illustration5,
+//   illustration6,
+// ];
+
+// const charactersPerFrameArray = [50, 30, 20]; 
+
+// const Introduction = (props) => {
+//   console.log("Introduction props", props);
+//   const { passageIdfordb, introductionPassages, handleNextComponent } = props;
+
+//   const [displayText, setDisplayText] = useState('');
+//   const [currentIndex, setCurrentIndex] = useState(0);
+//   const [currentIllustration, setCurrentIllustration] = useState(illustrations[0]);
+//   // const [introductionPassages, setIntroductionPassages] = useState([]);
+//   const [passageId, setPassageId] = useState(1);
+//   const [showNextButton, setShowNextButton] = useState(true);
+
+//   const [isMusicPlaying, setIsMusicPlaying] = useState(true);
+//   // const { handleNextComponent, passageIdfordb } = useContext(AppContext);
+//   const formatTextWithLineBreaks = (text) => {
+//     const formattedText = text.replace(/<br>/g, '<br/>');
+//     return formattedText;
+//   };
+
+//   const textRef = React.useRef(null);
+
+
+//   useEffect(() => {
+//     const passageText = introductionPassages?.find((passage) => passage.id === passageId)?.passage_text || '';
+//     console.log("Passage Text:", passageText); // Ajoutez cette ligne pour vérifier le texte du passage
+//     console.log("testttttt3", passageText);
+
+//     const intervalId = setInterval(() => {
+//       const charactersPerFrame = charactersPerFrameArray.find((value, index) => index === passageId - 1) || 100;
+//       if (currentIndex < passageText.length) {
+//         setDisplayText((prevText) => prevText + passageText[currentIndex]);
+//         setCurrentIndex((prevIndex) => prevIndex + 1);
+//       } else {
+//         clearInterval(intervalId);
+//         if (currentIllustration !== illustrations[illustrations.length - 1]) {
+//           const DELAY_BEFORE_NEXT_PASSAGE = 6700;
+//           setTimeout(() => {
+//             const currentIllustrationIndex = illustrations.indexOf(currentIllustration);
+//             setCurrentIllustration(illustrations[currentIllustrationIndex + 1]);
+//             setCurrentIndex(0);
+//             setDisplayText('');
+//             setPassageId((prevPassageId) => prevPassageId + 1);
+//           }, DELAY_BEFORE_NEXT_PASSAGE);
+//         } else {
+//           setShowNextButton(true); 
+//         }
+//       }
+//     }, 75);
+
+//     return () => clearInterval(intervalId);
+//   }, [currentIndex, currentIllustration, introductionPassages, passageId]);
+
+//   useEffect(()=> {
+//     textRef.current.scrollTop = textRef.current.scrollHeight
+//   },[displayText])
+
+//   return (
+//     <div className="container">
+//       <div className="illustration-container" style={{ backgroundImage: `url(${currentIllustration})` }} />
+//       <div className="text-container">
+//         <div className="parchment aaa" id="parchment-id" ref={textRef}>
+//           <p className="text"  dangerouslySetInnerHTML={{ __html: formatTextWithLineBreaks(displayText)   }} />
+//         {showNextButton && <button onClick= {handleNextComponent} className="next-button">Next</button>}
+//         </div>
+//       </div>
+//       <audio src="/RPG intro 100 BPM2.wav" autoPlay={isMusicPlaying} />
+//     </div>
+//   );
+// };
+
+// export default Introduction;
